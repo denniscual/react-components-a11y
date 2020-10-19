3f85ca139763da7703b50368dbcf046b6641694e
Dennis Cual <mamaylovepapay@outlook.com>
2020-10-19 20:11:24 +0800

    chore: init layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styles from './App.module.css'
+import { Accordion } from './components/Accordion'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.Container}>
+      <div className={styles.Content}>
+        <Accordion />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Accordion.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './Accordion.module.css'
+
+function Accordion() {
+  return <div>Accordion</div>
+}
+
+export { Accordion }
