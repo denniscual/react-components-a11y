b7302984514b94550ed74c41b23910cb10494fda
Dennis Cual <mamaylovepapay@outlook.com>
2020-10-20 02:04:52 +0800

    chore(single-accordion): change the accordion name to SingleAccordion and support diff types

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,45 @@
 import React from 'react'
 import styles from './App.module.css'
-import { TabbedAccordion } from './components/Accordion'
+import { SingleAccordion, SingleAccordionTypes } from './components/Accordion'
 
 function App() {
   return (
     <div className={styles.Container}>
       <div className={styles.Content}>
-        <TabbedAccordion activeIdx={1}>
-          <TabbedAccordion.Item idx={0}>
+        <SingleAccordion activeIdx={0} type={SingleAccordionTypes.collapsible}>
+          <SingleAccordion.Item idx={0}>
             <h3>
-              <TabbedAccordion.Button
+              <SingleAccordion.Button
                 id="accordion-button-zion"
                 aria-controls="accordion-panel-zion"
               >
                 Zion biography
-              </TabbedAccordion.Button>
+              </SingleAccordion.Button>
             </h3>
-            <TabbedAccordion.Panel
+            <SingleAccordion.Panel
               id="accordion-panel-zion"
               aria-labelledby="accordion-button-zion"
             >
               <p>Zion is the son of the great Irish and Dennis</p>
-            </TabbedAccordion.Panel>
-          </TabbedAccordion.Item>
-          <TabbedAccordion.Item idx={1}>
+            </SingleAccordion.Panel>
+          </SingleAccordion.Item>
+          <SingleAccordion.Item idx={1}>
             <h3>
-              <TabbedAccordion.Button
+              <SingleAccordion.Button
                 id="accordion-button-irish"
                 aria-controls="accordion-panel-irish"
               >
                 Irish biography
-              </TabbedAccordion.Button>
+              </SingleAccordion.Button>
             </h3>
-            <TabbedAccordion.Panel
+            <SingleAccordion.Panel
               id="accordion-panel-irish"
               aria-labelledby="accordion-button-irish"
             >
               <p>Irish is the son of the great Berto and Chengly</p>
-            </TabbedAccordion.Panel>
-          </TabbedAccordion.Item>
-        </TabbedAccordion>
+            </SingleAccordion.Panel>
+          </SingleAccordion.Item>
+        </SingleAccordion>
       </div>
     </div>
   )
--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import styles from './Accordion.module.css'
 
 /**
  * This module must expose a single Object which has Components `Group`, `Item`, `Button`, and `Panel`.
@@ -8,7 +7,7 @@ import styles from './Accordion.module.css'
  * */
 
 /**
- * What we gonna do is to have 2 types of Accordion. The "tabbed" and "multiselect".
+ * What we gonna do is to have 2 types of Accordion. The "single" and "multiselect".
  * */
 
 /**
@@ -17,19 +16,32 @@ import styles from './Accordion.module.css'
 
 // ---------------------------------------- //
 // ---------------------------------------- //
-// Tabbed
+// Single Accordion
+//
+// The Accordion only shows 0 or 1 panel at a time. This is distinguished by the `type`.
+// As default, SingleAccordion will always shows 1 panel a time. But this can be change
+// by passing a `type` value `collapsible`. In this behaviour, the Accordion can show atleast 0 or 1 panel.
 // -----------------------------------------//
 // -----------------------------------------//
 
-const TabbedAccordionContext = React.createContext<
-  [number, React.Dispatch<React.SetStateAction<number>>] | undefined
+enum SingleAccordionTypes {
+  tabbed = 'tabbed',
+  collapsible = 'collapsible',
+}
+
+const SingleAccordionContext = React.createContext<
+  | {
+      activeIdx: [number, React.Dispatch<React.SetStateAction<number>>]
+      type: SingleAccordionTypes
+    }
+  | undefined
 >(undefined)
 
-function useTabbedAccordionCtx() {
-  const ctx = React.useContext(TabbedAccordionContext)
+function useSingleAccordionCtx() {
+  const ctx = React.useContext(SingleAccordionContext)
   if (typeof ctx === 'undefined') {
     throw new Error(
-      `Consuming "TabbedAccordionContext" outside its "Provider" can lead to error.`
+      `Consuming "SingleAccordionContext" outside its "Provider" can lead to error.`
     )
   }
   return ctx
@@ -57,52 +69,77 @@ function AccordionItem({ idx, children }: AccordionItemProps) {
   )
 }
 
-function AccordionButton(props: JSX.IntrinsicElements['button']) {
+function AccordionButton(
+  props: Omit<JSX.IntrinsicElements['button'], 'onClick'>
+) {
   const accordionItemCtx = useAccordionItemCtx()
-  const [activeIdx, setActiveIdx] = useTabbedAccordionCtx()
+  const { activeIdx: activeIdxState, type } = useSingleAccordionCtx()
+  const [activeIdx, setActiveIdx] = activeIdxState
 
   const isActive = accordionItemCtx === activeIdx
 
+  // The aria props are different based on the type. If the type is `collapsible`,
+  // we will not add `aria-disabled` to the button.
+  const ariaProps =
+    type === SingleAccordionTypes.tabbed
+      ? {
+          'aria-expanded': isActive,
+          'aria-disabled': isActive,
+        }
+      : { 'aria-expanded': isActive }
+
   function handleClick() {
-    setActiveIdx(accordionItemCtx)
+    if (type === SingleAccordionTypes.tabbed) {
+      setActiveIdx(accordionItemCtx)
+    }
+    // If collapsible
+    else {
+      if (isActive) {
+        setActiveIdx(-1)
+      } else {
+        setActiveIdx(accordionItemCtx)
+      }
+    }
   }
 
-  return (
-    <button
-      {...props}
-      onClick={handleClick}
-      aria-expanded={isActive}
-      aria-disabled={isActive}
-    />
-  )
+  return <button {...props} onClick={handleClick} {...ariaProps} />
 }
 
 function AccordionPanel(props: JSX.IntrinsicElements['div']) {
   const accordionItemCtx = useAccordionItemCtx()
-  const [activeIdx] = useTabbedAccordionCtx()
+  const [activeIdx] = useSingleAccordionCtx().activeIdx
 
   return (
     <div role="region" {...props} hidden={accordionItemCtx !== activeIdx} />
   )
 }
 
-function TabbedAccordion({
+function SingleAccordion({
   activeIdx = 0,
+  type = SingleAccordionTypes.tabbed,
   children,
   ...otherProps
 }: {
   activeIdx?: number
+  type?: SingleAccordionTypes
 } & JSX.IntrinsicElements['div']) {
-  const accordionState = React.useState(activeIdx)
+  const activeIdxState = React.useState(activeIdx)
+  const value = React.useMemo(
+    () => ({
+      activeIdx: activeIdxState,
+      type,
+    }),
+    [activeIdxState, type]
+  )
   return (
-    <TabbedAccordionContext.Provider value={accordionState}>
+    <SingleAccordionContext.Provider value={value}>
       <div {...otherProps}>{children}</div>
-    </TabbedAccordionContext.Provider>
+    </SingleAccordionContext.Provider>
   )
 }
 
-TabbedAccordion.Item = AccordionItem
-TabbedAccordion.Button = AccordionButton
-TabbedAccordion.Panel = AccordionPanel
+SingleAccordion.Item = AccordionItem
+SingleAccordion.Button = AccordionButton
+SingleAccordion.Panel = AccordionPanel
 
-export { TabbedAccordion }
+export { SingleAccordion, SingleAccordionTypes }
