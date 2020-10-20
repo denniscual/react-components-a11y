9796e449a6986099d323125dd9ab459e2d01b80a
Dennis Cual <mamaylovepapay@outlook.com>
2020-10-20 23:47:08 +0800

    chore(single-accordion): tweak the tree elements handling logic and add support for up and down keys keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,23 @@ function App() {
   return (
     <div className={styles.Container}>
       <div className={styles.Content}>
-        <SingleAccordion id="biography-accordion" activeIdx={0}>
-          <SingleAccordion.Item idx={0}>
-            <h3>
-              <SingleAccordion.Button>Zion biography</SingleAccordion.Button>
-            </h3>
-            <SingleAccordion.Panel role="region">
-              <h4>My biography</h4>
-              <p>Zion is the son of the great Irish and Dennis</p>
-            </SingleAccordion.Panel>
-          </SingleAccordion.Item>
-          <SingleAccordion.Item idx={1}>
+        <SingleAccordion>
+          <SingleAccordion.Item>
             <h3>
               <SingleAccordion.Button>Irish biography</SingleAccordion.Button>
             </h3>
-            <SingleAccordion.Panel role="region">
+            <SingleAccordion.Panel>
               <h4>My biography</h4>
-              <p>Irish is the son of the great Berto and Chengly</p>
+              <p>Irish is the son of the great Berto and Chenglylyly</p>
+            </SingleAccordion.Panel>
+          </SingleAccordion.Item>
+          <SingleAccordion.Item>
+            <h3>
+              <SingleAccordion.Button>Zion biography</SingleAccordion.Button>
+            </h3>
+            <SingleAccordion.Panel>
+              <h4>My biography</h4>
+              <p>Zion is the son of the great Irish and Dennis</p>
             </SingleAccordion.Panel>
           </SingleAccordion.Item>
         </SingleAccordion>
