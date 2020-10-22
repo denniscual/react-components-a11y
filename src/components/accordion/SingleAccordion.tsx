dd5360b9440c3fb39e6ea522b9ed357a7d172b8b
Dennis Cual <mamaylovepapay@outlook.com>
2020-10-22 23:44:31 +0800

    chore(single-accordion): finalise the code docs

--- src/components/accordion/SingleAccordion.tsx
+++ src/components/accordion/SingleAccordion.tsx
@@ -7,18 +7,23 @@ import {
   forwardRefWithAs,
 } from '../../utils'
 
-// TODO: Add documentation from the accessiblity.
-
-// ---------------------------------------- //
-// ---------------------------------------- //
-// Single Accordion
-//
-// The Accordion only shows 0 or 1 panel at a time. This is distinguished by the `type`.
-// As default, SingleAccordion will always shows 1 panel a time. But this can be change
-// by passing a `type` value `collapsible`. In this behaviour, the Accordion can show atleast 0 or 1 panel and also
-// the active panel can collapse itself.
-// -----------------------------------------//
-// -----------------------------------------//
+/**
+ * Single Accordion
+ *
+ * An accordion is a vertically stacked set of interactive headings that each contain a
+ * title, content snippet, or thumbnail representing a section of content. The headings
+ * function as controls that enable users to reveal or hide their associated sections of content.
+ * Accordions are commonly used to reduce the need to scroll when presenting multiple sections of
+ * content on a single page.
+ *
+ * The Accordion only shows 0 or 1 panel at a time. This is distinguished by the `type`.
+ * As default, SingleAccordion will always shows 1 panel a time. But this can be change
+ * by passing a `type` value `collapsible`. In this behaviour, the Accordion can show atleast 0 or 1 panel and also
+ * the active panel can collapse itself.
+ *
+ * Specs:
+ *   - WAI-ARIA: https://www.w3.org/TR/wai-aria-practices-1.2/#accordion
+ * */
 
 const {
   DescendantsProvider,
@@ -31,12 +36,9 @@ enum SingleAccordionTypes {
   collapsible = 'collapsible',
 }
 
-type SingleAccordionProps = {
-  activeIdx?: number
-  type?: SingleAccordionTypes
-  id?: string
-}
-
+/**
+ *  This Component provdes information like `activeIdx` on its Consumer Components.
+ * */
 const SingleAccordion = forwardRefWithAs<
   HTMLDivElement,
   SingleAccordionProps,
@@ -63,11 +65,17 @@ const SingleAccordion = forwardRefWithAs<
   return (
     <DescendantsProvider>
       <SingleAccordionContext.Provider value={value}>
-        <Comp ref={forwardRef} {...otherProps} />
+        <Comp {...otherProps} ref={forwardRef} />
       </SingleAccordionContext.Provider>
     </DescendantsProvider>
   )
 })
+
+type SingleAccordionProps = {
+  activeIdx?: number
+  type?: SingleAccordionTypes
+  id?: string
+}
 
 function useSingleAccordionCtx() {
   const ctx = React.useContext(SingleAccordionContext)
@@ -88,6 +96,12 @@ const SingleAccordionContext = React.createContext<
   | undefined
 >(undefined)
 
+/**
+ * A Component which holds the index of the accordion button and its associated
+ * panel. This Component define the id name of the Button and the Panel.
+ * Also internally, the Component register an element, in this case the Button,
+ * on the `SingleAccordion` Descendants collection.
+ * */
 const SingleAccordionItem = forwardRefWithAs<
   HTMLDivElement,
   {},
@@ -140,13 +154,15 @@ const AccordionItemContext = React.createContext<
 >(undefined)
 
 /**
- *  Each accordion header `button` is wrapped in an element with role
- *  `heading` that has a value set for `aria-level` that is appropriate
- *  for the information architecture of the page.
- *  https://www.w3.org/TR/wai-aria-practices-1.2/#accordion
- *  I believe this should be left for apps to handle, since headings
- *  are necessarily context-aware. An app can wrap a button inside any
- *  arbitrary tag(s).
+ * The title of each accordion header is contained in an element with role button.
+ *
+ * Each accordion header `button` is wrapped in an element with role
+ * `heading` that has a value set for `aria-level` that is appropriate
+ * for the information architecture of the page.
+ * https://www.w3.org/TR/wai-aria-practices-1.2/#accordion
+ * I believe this should be left for apps to handle, since headings
+ * are necessarily context-aware. An app can wrap a button inside any
+ * arbitrary tag(s).
  *
  * @example
  * <div>
@@ -224,9 +240,9 @@ const SingleAccordionButton = forwardRefWithAs<HTMLButtonElement, {}, 'button'>(
 
     return (
       <Comp
-        ref={ref}
         {...otherProps}
         {...ariaProps}
+        ref={ref}
         id={buttonId}
         onClick={wrappedClickHandler}
         onKeyDown={wrappedKeyDownHandler}
@@ -260,18 +276,23 @@ const SingleAccordionPanel = forwardRefWithAs<
 
   return (
     <Comp
-      ref={forwardRef}
       {...otherProps}
+      ref={forwardRef}
+      /**
+       * Optionally, each element that serves as a container for panel content has role region
+       * and aria-labelledby with a value that refers to the button that controls display of the panel.
+       * https://www.w3.org/TR/wai-aria-practices-1.2/#wai-aria-roles-states-and-properties
+       * */
+      role="region"
+      aria-labelledby={buttonId}
       /**
        * Some screen readers when using "Up" and "Down" keys, it will not
        * jump the focus on next accordion header, but will jump to the active panel.
        * Because of this, we will make this "div" focusable.
        * */
       tabIndex={-1}
-      aria-labelledby={buttonId}
       id={panelId}
       hidden={idx !== activeIdx}
-      role="region"
     />
   )
 })
