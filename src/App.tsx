6b99a5908ea1fca77b53e81f96c49be16acf8df7
Dennis Cual <mamaylovepapay@outlook.com>
2020-10-29 23:49:41 +0800

    chore(dialog): add logic for closing the dialog via clicking to overlay

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './App.module.css'
 import Dialog from './components/dialog'
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  function handleCloseDialog() {
+    setIsOpen(false)
+  }
+
   return (
     <div className={styles.Container}>
       <div className={styles.Content}>
-        <Dialog aria-labelledby="dialog-title" aria-describedby="dialog-desc">
-          <button>Close</button>
+        <button onClick={() => setIsOpen(true)}>Open dialog</button>
+        <Dialog
+          isOpen={isOpen}
+          onClose={handleCloseDialog}
+          aria-labelledby="dialog-title"
+          aria-describedby="dialog-desc"
+        >
+          <button onClick={handleCloseDialog}>Close</button>
           <h1 id="dialog-title">Dialog title</h1>
           <p id="dialog-desc">Dialog description</p>
         </Dialog>
       </div>
+      <p>
+        Soluta excepturi praesent cursus eos, varius beatae hymenaeos, neque
+        ipsa proin dictumst perferendis quibusdam laoreet diamlorem nostra
+        necessitatibus eaque iaculis cras rerum pede deleniti, cupidatat
+        distinctio, ante conubia volutpat repudiandae? Distinctio dapibus
+        viverra aliquam morbi magni repudiandae dapibus labore cubilia. Sit
+        eveniet, nunc quod ipsam. At autem exercitationem ipsa ullamco molestie,
+        accusantium per taciti sit numquam, duis tellus morbi praesentium,
+        diamlorem hendrerit, metus. Facilisi? Massa, ullamco ultrices iure esse
+        eu nostra tortor, nihil volutpat accumsan nostra minima pretium, ab
+        ipsum diam. Gravida reiciendis quisquam, praesentium delectus, nisl
+        velit? Potenti corporis, amet? Sociis diam venenatis penatibus
+        fermentum. Libero eligendi. Vel egestas.
+      </p>
     </div>
   )
 }
--- src/components/dialog/Dialog.tsx
+++ src/components/dialog/Dialog.tsx
@@ -41,33 +41,88 @@ function Portal({ as = 'div', children }: PropsWithChildren<{ as?: string }>) {
   return parentEl.current ? createPortal(children, parentEl.current) : null
 }
 
-const Dialog = forwardRefWithAs<HTMLDivElement, {}, 'div'>(function Dialog(
-  { as: Comp = 'div', children, ...otherProps },
-  forwardRef
-) {
+const Dialog = forwardRefWithAs<HTMLDivElement, DialogProps, 'div'>(
+  function Dialog(
+    { as: Comp = 'div', isOpen, onClose, ...otherProps },
+    forwardRef
+  ) {
+    const inertContainerRef = React.useRef<HTMLDivElement | null>(null)
+    const mouseDownEventTargetRef = React.useRef<EventTarget | null>(null)
+
+    /**
+     * We use the handleMouseDown and handleClick handlers for closing the modal
+     * when clicking to the element. THe order of execution is `onMouseDown` then `onClick`. So what we did is
+     * in `handleMouseDown`, we will get the current target object which points to the element to which this event handler
+     * has been attached.
+     * THen on the `handleClick` we will check where the event is fired. If the event is fired on the overlay element, then
+     * we wll invoke the `onClose`. Else, prevent the closing of modal.
+     * */
+    const handleMouseDown: React.MouseEventHandler<HTMLDivElement> = (
+      event
+    ) => {
+      mouseDownEventTargetRef.current = event.currentTarget
+    }
+
+    const handleClick: React.MouseEventHandler<HTMLDivElement> = (event) => {
+      if (mouseDownEventTargetRef.current === event.target) {
+        event.stopPropagation()
+        onClose()
+      }
+    }
+
+    const handleKeyDown: React.MouseEventHandler<HTMLDivElement> = (event) => {}
+
+    return isOpen ? (
+      <Portal>
+        <FocusOn returnFocus>
+          <div
+            onClick={handleClick}
+            onMouseDown={handleMouseDown}
+            ref={inertContainerRef}
+            className={styles.InertContainer}
+          >
+            <Comp
+              {...otherProps}
+              ref={forwardRef}
+              className={styles.DialogContainer}
+              /**
+               * The element that serves as the dialog container has a role of dialog.
+               * https://www.w3.org/TR/wai-aria-practices-1.2/#dialog_roles_states_props
+               * */
+              role="dialog"
+              /**
+               * The dialog container element has aria-modal set to true.
+               * https://www.w3.org/TR/wai-aria-practices-1.2/#dialog_roles_states_props
+               * */
+              aria-modal={true}
+            />
+          </div>
+        </FocusOn>
+      </Portal>
+    ) : null
+  }
+)
+
+interface DialogProps {
+  isOpen: boolean
+  onClose(): void
+}
+
+/**
+ * Windows under a modal dialog are inert. That is, users cannot interact with content outside
+ * an active dialog window. Inert content outside an active dialog is typically visually obscured
+ * or dimmed so it is difficult to discern, and in some implementations, attempts to
+ * interact with the inert content cause the dialog to close.
+ *
+ * */
+const InertContainer = function InertContainer({
+  children,
+}: PropsWithChildren<{}>) {
   return (
     <Portal>
-      <FocusOn className={styles.DialogOverlay}>
-        <Comp
-          {...otherProps}
-          ref={forwardRef}
-          className={styles.DialogContainer}
-          /**
-           * The element that serves as the dialog container has a role of dialog.
-           * https://www.w3.org/TR/wai-aria-practices-1.2/#dialog_roles_states_props
-           * */
-          role="dialog"
-          /**
-           * The dialog container element has aria-modal set to true.
-           * https://www.w3.org/TR/wai-aria-practices-1.2/#dialog_roles_states_props
-           * */
-          aria-modal={true}
-        >
-          {children}
-        </Comp>
-      </FocusOn>
+      <div className={styles.InertContainer}>{children}</div>
     </Portal>
   )
-})
+}
 
 export { Dialog as default }
